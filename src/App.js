368e47669520c7bb8c2c908160dfc54d43b3443a
ahlem-phantom <ahlem.laajili@esprit.tn>
2022-03-05 20:03:07 +0100

    workshop-1-done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const menu = (<div>
+    <ul id="nav">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">FAQ</a></li>
+    <li><a href="#">Contact</a></li>
+    </ul>
     </div>
+     );
+    
+  return (
+  <div>
+     {menu}
+     <div>
+     [Page content here]
+     </div>
+</div>
+
   );
 }
 
